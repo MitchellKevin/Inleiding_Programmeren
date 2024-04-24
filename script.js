69fc184d8d6d78cf7de43c71a4cb2ba3a6fe321b
Mitchell Scholte <mitchell.scholte04@gmail.com>
2024-04-24 11:07:22 +0200

    update

--- script.js
+++ script.js
@@ -6,8 +6,20 @@ const button= document.querySelector("button")
 function veranderKnopNaam(){
     kop.textContent=bericht
 }
+// function veranderKnopNaam2(){
+//     kop.textContent=bericht2
+//     setAmount=2
+// }
 console.log(kop)
 button.addEventListener("click", veranderKnopNaam)
+
+// if(setAmount==1){
+//     button.addEventListener("click", veranderKnopNaam)
+// }
+// else{
+//     button.addEventListener("click", veranderKnopNaam2)
+// }
+
 // let level="1"
 // let naam="Mario"
 
