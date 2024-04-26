9e266d92dde3a0c9f8a20cb8e9a09e715aa241c5
Mitchell Scholte <mitchell.scholte04@gmail.com>
2024-04-26 10:40:15 +0200

    dobbelsteen opdracht wk1 klaar

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// wk1
+// eerste oefening
 let kop=document.querySelector("h1")
 let bericht ="Bedankt!!"
 
@@ -28,3 +30,30 @@ button.addEventListener("click", veranderKnopNaam)
 
 // console.log(level,naam,scoreMin,scoreMax)
 
+// Dobbelsteen oefening
+function reRoll(){
+var aantalOgen = Math.ceil(Math.random()*6);
+
+// even in de console kijken welke waarde hier uitkomt
+console.log(aantalOgen);
+
+//we schrijven deze waarde ook naar de <p>aragraaf in de html 
+document.querySelector("p").textContent = 'mooie worp: '+aantalOgen; 
+
+// een verzameling met alle bestandsnamen van de afbeeldingen uit het mapje images aanmaken
+var plaatjesArray = ['one.png','two.png','three.png','four.png','five.png','six.png'];
+
+var dobbelsteenAfbeelding = plaatjesArray[aantalOgen-1];
+
+document.querySelector("img").src = '../image/'+dobbelsteenAfbeelding;
+if (aantalOgen == 6){
+    document.querySelector('p').textContent = "Feest! Dubbel uitbetaald"
+    }
+else if(aantalOgen != 4 && aantalOgen!=2){
+        document.querySelector('p').textContent="Jammer, oneven. Je hebt verloren"
+    }
+else{
+        document.querySelector('p').textContent="GEFELICITEERD, je hebt gewonnen"
+    }
+}
+button.addEventListener("click",reRoll)
