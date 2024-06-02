4633efa7503c8f4bf8239947f989012e5f3c94cf
Mitchell Scholte <mitchell.scholte04@gmail.com>
2024-06-02 20:05:29 +0200

    oude code verwijderd

--- eindopdracht/mario.js
+++ eindopdracht/mario.js
@@ -12,10 +12,6 @@ let bombaAfstand_2 = -670;
 const enemy1 = document.getElementById("enemy1");   
 let bombaAfstand_1 = -1520;
 let hs=document.querySelector("h2");
-// let isJumping = false;
-// let gravity = 0.5;
-// let velocityY = 0;
-// const groundLevel = 355;
 let box0=document.querySelector(".box0");
 let box1= document.querySelector(".box1");
 let box3=document.querySelector(".box3");
@@ -32,19 +28,15 @@ function start(){
       goombaStart()
   document.body.onkeydown = function userData(e) {
       if( e.keyCode == 32 ) {
-        jump();
-        // mario.classList.add("large")
-        // setTimeout(function(){mario.classList.remove("large")}, 500)      
+        jump();     
       }else if(e.keyCode==68){
           mario.classList.add("right")
           setTimeout(function(){mario.classList.remove("left")})
           goRight();
-          // mario.classList.add("right")
       }else if(e.keyCode==65){
           mario.classList.add("left")
           setTimeout(function(){mario.classList.remove("right")})
           goLeft();
-          // mario.classList.add("left")
       }
       enemyCollisionChecker()
       boxCollisioChecker()
@@ -56,7 +48,6 @@ function start(){
 
 function jump() {
   // https://www.basedash.com/blog/how-to-change-an-image-src-with-javascript
-    // document.getElementById('mario').height="500";
     setTimeout(function(){
       document.getElementById("mario").src = "mario.webp";
    }, 500);
@@ -65,66 +56,21 @@ function jump() {
     setTimeout(function(){mario.classList.remove("jump")}, 500)
   
   }
-
-
-  // function applyGravity() {
-  //   if (isJumping) {
-  //       velocityY += gravity;
-  //       mario.style.top = (mario.offsetTop + velocityY) + 'px';
-
-  //       // Check for collision with the ground
-  //       if (mario.offsetTop >= groundLevel) {
-  //           mario.style.top = groundLevel + 'px';
-  //           isJumping = false;
-  //           mario.classList.remove("jump");
-  //           mario.src = "mario.webp";
-  //       }
-
-  //       // Check for collision with box1
-  //       if (collisionCheckBox1()) {
-  //           isJumping = false;
-  //           mario.classList.remove("jump");
-  //           mario.src = "mario.webp";
-  //           mario.style.top = (box1.offsetTop - mario.offsetHeight) + 'px';
-  //       }
-  //   }
-  //   requestAnimationFrame(applyGravity);
-  // }
   
 function goRight() {
-    // mario.classList.add("right")
     locaAddRight()
     const test=document.getElementById("mario")
     test.style.left=loca + 'px'
-    // document.getElementById('mario').height="500";
     console.log("Right pressed")
-    // setTimeout(function(){mario.classList.remove("right")}, 500)
   }
-  
-  
-//   document.body.onkeyup = function(e) {
-//     if( e.keyCode == 37 ) {
-//       goLeft();
-//       mario.classList.add("left")
-//     //   setTimeout(function(){mario.classList.remove("right")}, 500)
-      
-//     }
-//   }
+
 function goLeft() {
-    // document.getElementById('mario').height="500";
-    // mario.classList.add("left")
     locaAddLeft()
     const test=document.getElementById("mario")
     test.style.left=loca + 'px'
     console.log("Left pressed")
-    // setTimeout(function(){mario.classList.remove("left")}, 500)
   }
 
-
-
-// function locaGet(){
-//   console.log("Loca: " + loca)
-// }
 // https://www.w3schools.com/js/js_htmldom_css.asp
 function locaAddLeft(){
   loca= loca - 20
@@ -171,30 +117,7 @@ function goombaStart(){
   bomba_1_Loca()
   bomba_2_Loca()
 }
-    // for (let i = 0; i < 10; i++) {
-    //   const bomba_1=document.getElementById("enemy1")
-    //   bombaAfstand=i*300
-    //   bomba_1.style.left=bombaAfstand+'px' 
-    // }
 
-// function collisionCheck(){
-//   collisionCheckBox0()
-//   collisionCheckBox1()
-//   collisionCheckBox4()
-// }
-
-// function collisionEnemy(){
-//   collisionCheckEnemy1()
-//   collisionCheckEnemy2()
-// }
-
-// if(loca==succes){
-//   restartLoc()
-// }
-
-// function restartLoc(){
-//   loca=loca-1000
-// }
 function boxCollisioChecker(){
   if(mario.x<=437){
     collisionCheckBox1()
@@ -215,15 +138,6 @@ function enemyCollisionChecker(){
   }
 }
 
-
-// function boxCollisionChecker2() {
-//   if (mario.x > 812 && mario.x < 1512) {
-//       collisionCheckBox4();
-//   } else if (mario.x > 1512) {
-//       collisionCheckBox3()
-//   }
-// }
-
 function collisionCheckBox1(){
 
   if(mario.x > 337 + box1.offsetWidth ||
@@ -234,12 +148,8 @@ function collisionCheckBox1(){
     {
       console.log("geen collision")
       setTimeout(function(){mario.classList.remove("block")}, 100)
-      // const block=document.getElementById("mario")
-      // block.style.top= 355 + 'px'
     }else{
       console.log('Collision')
-      // const block=document.getElementById("mario")
-      // block.style.top=100 + 'px'
       mario.classList.add("block")
     }
   }
@@ -253,37 +163,13 @@ function collisionCheckBox1(){
         {
           console.log("geen collision")
           setTimeout(function(){mario.classList.remove("block")}, 100)
-          // const block=document.getElementById("mario")
-          // block.style.top= 355 + 'px'
         }else{
           console.log('Collision')
-          // const block=document.getElementById("mario")
-          // block.style.top=100 + 'px'
           mario.classList.add("block")
         }
 
 
 }
-// function collisionCheckBox2(){
-
-//   if(mario.x > 337 + box1.offsetWidth ||
-//     mario.x + mario.offsetWidth < 337 ||
-//     mario.y > 611 + box1.offsetHeight ||
-//     mario.y + mario.offsetHeight < 611)
-//     {
-//       console.log("geen collision")
-//       setTimeout(function(){mario.classList.remove("block")}, 100)
-//       // const block=document.getElementById("mario")
-//       // block.style.top= 355 + 'px'
-//     }else{
-//       console.log('Collision')
-//       // const block=document.getElementById("mario")
-//       // block.style.top=100 + 'px'
-//       mario.classList.add("block")
-//     }
-
-
-// }
 function collisionCheckBox3(){
 
   if(mario.x > 1612 + box3.offsetWidth ||
@@ -293,12 +179,8 @@ function collisionCheckBox3(){
     {
       console.log("geen collision")
       setTimeout(function(){mario.classList.remove("block")}, 100)
-      // const block=document.getElementById("mario")
-      // block.style.top= 355 + 'px'
     }else{
       console.log('Collision')
-      // const block=document.getElementById("mario")
-      // block.style.top=100 + 'px'
       mario.classList.add("block")
     }
 
@@ -313,12 +195,9 @@ function collisionCheckBox4(){
     {
       console.log("geen collision")
       setTimeout(function(){mario.classList.remove("block")}, 100)
-      // const block=document.getElementById("mario")
-      // block.style.top= 355 + 'px'
+
     }else{
       console.log('Collision')
-      // const block=document.getElementById("mario")
-      // block.style.top=100 + 'px'
       mario.classList.add("block")
     }
 
@@ -326,10 +205,6 @@ function collisionCheckBox4(){
 }
 
 function collisionCheckEnemy2(){
-  
-  // var mario={x:500, y:355, width:50, height:50};
-  // var box1={x:260, y:325, width:30, height:30};
-
   if(mario.x > enemy2.x + enemy2.offsetWidth ||
     mario.x + mario.offsetWidth < enemy2.x ||
     mario.y > enemy2.y + box1.offsetHeight ||
@@ -343,9 +218,6 @@ function collisionCheckEnemy2(){
 
 }
 function collisionCheckEnemy1(){
-  
-  // var mario={x:500, y:355, width:50, height:50};
-  // var box1={x:260, y:325, width:30, height:30};
 
   if(mario.x > enemy1.x + enemy1.offsetWidth ||
     mario.x + mario.offsetWidth < enemy1.x ||
@@ -359,24 +231,6 @@ function collisionCheckEnemy1(){
     }
 
 }
-// Mario
-
-// position: absolute;
-// top: 355px;
-// left: 500px;
-// width: 50px;
-// height: 50px;
-// transition: all 0.2s;
-// border: red 1px solid;
-
-// box 1
-
-// position: absolute;
-// width: 30px;
-// height: 30px;
-// left: 260px;
-// top: 325px;
-// border: red 5px solid;
 
 function finish(){
   if(loca==1400){
